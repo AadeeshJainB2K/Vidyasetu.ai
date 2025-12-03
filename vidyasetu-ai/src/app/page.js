c91cb5b57ef63e322afd04c842a039ca8e39f4be
Aadeesh Jain <aadeeshjain15@gmail.com>
2025-12-03 18:11:57 +0530

    Add Header component and integrate it into Home page layout

--- vidyasetu-ai/src/app/page.js
+++ vidyasetu-ai/src/app/page.js
@@ -1,10 +1,6 @@
-export default function Home() {
-  return (
-    <>
-      <h1>
-        Hello Aadeesh , Naman in bracnch aadeesh j, my name is aadeesh jai
-      </h1>
-    </>
-  );
+import { redirect } from 'next/navigation'
+
+export default function Page() {
+  redirect('/home')
+  return null
 }
-//https://meet.google.com/ryi-pqdc-nma
