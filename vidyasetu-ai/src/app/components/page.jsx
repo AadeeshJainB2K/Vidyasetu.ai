5074c223925ddf86c03f26f9c554fe6054b82990
Aadeesh Jain <aadeeshjain15@gmail.com>
2025-12-03 20:13:04 +0530

    cleaned the project for hackthon

--- vidyasetu-ai/src/app/components/page.jsx
+++
@@ -1,5 +0,0 @@
-import LoginForm from "../login";
-
-export default function LoginPage() {
-  return <LoginForm />;
-}
--- vidyasetu-ai/src/app/home/page.js
+++
@@ -1,9 +0,0 @@
-import Home from "../components/home/home";
-
-export default function Page() {
-  return (
-    <>
-      <Home />
-    </>
-  );
-}
