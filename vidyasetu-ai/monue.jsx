258967655abca7e94423a50039a2b1d86c4d0fbc
NaManThaKurZ <sunidhithakur1127@gmail.com>
2025-12-05 14:08:01 +0530

    +!

---
+++ vidyasetu-ai/monue.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/CSYa4tq3vmpiothS/scene.splinecode" 
+      />
+    </main>
+  );
+}
